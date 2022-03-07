2b5d3eb0afb2aa5f93e6abf990ffb549c9f3172d
LawrenceOL <loreganlloyd@gmail.com>
2022-03-07 15:04:50 -0500

    created function to seed hidden items

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,36 @@
 gameSquares = document.querySelectorAll('.game-square')
+squareCount = 81
+totalHidden = 10
+seededHidden = 0
+totalGems = 0
 
-squareCount = 0
+seedHidden()
+
 gameSquares.forEach((gameSquare) => {
   gameSquare.addEventListener('click', () => {
-    gameSquare.style.background = 'red'
+    gameSquare.style.background = 'green'
   })
 })
+
+//functions
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min) //The maximum is inclusive and the minimum is inclusive
+}
+
+//do I need break here?
+function seedHidden() {
+  while (seededHidden < totalHidden) {
+    gameSquares.forEach((gameSquare) => {
+      if (seededHidden < totalHidden) {
+        if (getRandomIntInclusive(1, 10) === 5) {
+          gameSquare.style.background = 'red'
+          seededHidden += 1
+        }
+      }
+    })
+  }
+}
