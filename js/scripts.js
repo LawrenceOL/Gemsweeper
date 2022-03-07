7b748b3b600ed82eb483e4a3ffb2808f19af3805
LawrenceOL <loreganlloyd@gmail.com>
2022-03-07 14:23:21 -0500

    added click event listener to each square

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 gameSquares = document.querySelectorAll('.game-square')
 
 squareCount = 0
-for (let i = 0; i < gameSquares.length; i++) {
-  squareCount += 1
-}
-alert(squareCount)
+gameSquares.forEach((gameSquare) => {
+  gameSquare.addEventListener('click', () => {
+    gameSquare.style.background = 'red'
+  })
+})
