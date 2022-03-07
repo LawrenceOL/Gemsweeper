7f37f634d4d14bcb2dcb07f5aebc0acbb0321e47
LawrenceOL <loreganlloyd@gmail.com>
2022-03-07 16:29:43 -0500

    added function to seed gems

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
-gameSquares = document.querySelectorAll('.game-square')
-resetButton = document.querySelector('button')
-squareCount = 81
-totalHidden = 10
-seededHidden = 0
-totalGems = 0
+let gameSquares = document.querySelectorAll('.game-square')
+let resetButton = document.querySelector('button')
+
+let lavaDisplay = document.querySelector('#lava-display')
+let gemDisplay = document.querySelector('#gem-display')
+
+let squareCount = 81
+let totalHidden = 10
+
+//could use seededSquares instead of seededHidden?
+let seededHidden = 0
+let totalGems = 2
+let seededGems = 0
 
 seedHidden()
+let seededSquares = document.querySelectorAll('.seeded-square')
+seedGems()
 
 gameSquares.forEach((gameSquare) => {
   gameSquare.addEventListener('click', () => {
@@ -24,22 +33,44 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min) //The maximum is inclusive and the minimum is inclusive
 }
 
-//do I need break here?
 function seedHidden() {
   while (seededHidden < totalHidden) {
     gameSquares.forEach((gameSquare) => {
       if (seededHidden < totalHidden) {
         if (getRandomIntInclusive(1, 10) === 5) {
           gameSquare.style.background = 'red'
+          gameSquare.classList.add('seeded-square')
           seededHidden += 1
+        }
+      }
+    })
+  }
+  lavaDisplay.innerText = `${
+    seededHidden - seededGems
+  } lava squares remain unmarked`
+}
+
+function seedGems() {
+  while (seededGems < totalGems) {
+    seededSquares.forEach((seededSquare) => {
+      if (seededGems < totalGems) {
+        if (getRandomIntInclusive(1, 10) === 5) {
+          seededSquare.style.background = 'lightblue'
+          seededGems += 1
+          seededSquare.classList.remove('seeded-square')
+          seededSquare.classList.add('seeded-gem')
         }
       }
     })
   }
 }
 
+//seed hidden gems
+
 function reset() {
   gameSquares.forEach((gameSquare) => {
+    gameSquare.classList.remove('seeded-square')
+    gameSquare.classList.remove('seeded-gem')
     if (
       gameSquare.style.background === 'red' ||
       gameSquare.style.background === 'green'
@@ -47,4 +78,7 @@ function reset() {
       gameSquare.style.background = 'gray'
     }
   })
+  seededHidden = 0
+  seededGems = 0
+  seedHidden()
 }
