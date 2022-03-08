980697586209378f6ea964b54d00a96d4bbb18a1
LawrenceOL <loreganlloyd@gmail.com>
2022-03-08 18:22:40 -0500

    restore previous functionality using new data structure

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ let lavaDisplay = document.querySelector('#lava-display')
 let gemDisplay = document.querySelector('#gem-display')
 
 let squareCount = 81
-let totalHiddenObjects = 10
+let totalLavas = 8
 
 //could use seededSquares instead of seededHiddenLava?
 let seededHiddenLava = 0
@@ -15,13 +15,15 @@ let seededGems = 0
 let coordinateArrayOfSquares = []
 
 const idToCoords = {}
+const squares = {}
 
-generateArray()
+generateCoordinatesArray()
+generateSquareObjects()
 addGreenClickAndBoxID()
-// seedHidden()
 
-// let seededSquares = document.querySelectorAll('.seeded-square')
-// seedGems()
+seedHidden()
+
+seedGems()
 
 resetButton.addEventListener('click', reset)
 
@@ -37,21 +39,33 @@ function getRandomIntInclusive(min, max) {
 //fix this function?
 
 function seedHidden() {
-  while (seededHiddenLava < totalHiddenObjects) {
-    gameSquares.forEach((seededSquare) => {
-      if (
-        seededHiddenLava < totalHiddenObjects &&
-        !(seededSquare.style.background = 'red')
-      ) {
+  while (seededHiddenLava < totalLavas) {
+    for (let i = 0; i < 81; i++) {
+      if (seededHiddenLava < totalLavas && !squares['box' + i].isLava) {
         if (getRandomIntInclusive(1, 10) === 5) {
-          seededSquare.style.background = 'red'
-
-          // !!!!!!!!!!! are there checks for .seeded-square?
-          // id.classList.add('seeded-square')
+          squares['box' + i].isLava = true
+          eval(squares['box' + i].selector + ".style.background = 'red'")
           seededHiddenLava += 1
         }
       }
-    })
+    }
+  }
+}
+
+function seedGems() {
+  while (seededGems < totalGems) {
+    for (let i = 0; i < 81; i++) {
+      if (
+        seededGems < totalGems &&
+        (!squares['box' + i].isGem || !squares['box' + i].isGem)
+      ) {
+        if (getRandomIntInclusive(1, 10) === 5) {
+          squares['box' + i].isGem = true
+          eval(squares['box' + i].selector + ".style.background = 'lightblue'")
+          seededGems += 1
+        }
+      }
+    }
   }
 }
 
@@ -76,7 +90,7 @@ function seedHidden() {
 // }
 
 //generates 9x9 2d array
-function generateArray() {
+function generateCoordinatesArray() {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       coordinateArrayOfSquares.push([i, j])
@@ -84,19 +98,37 @@ function generateArray() {
   }
 }
 
+function generateSquareObjects() {
+  for (let i = 0; i < squareCount; i++) {
+    let boxName = 'box' + i
+    squares['box' + i] = {
+      name: boxName,
+      coordinates: coordinateArrayOfSquares[i],
+      neighbors: 'tbd',
+      isLava: false,
+      isGem: false,
+      isFlagged: false,
+      isFlipped: false,
+      selector: `document.querySelector('.${boxName}')`
+    }
+  }
+}
+
 function addGreenClickAndBoxID() {
   gameSquares.forEach((gameSquare, index) => {
     let id = `box${index}`
+    gameSquare.classList.add(id)
+
     //adds box# tag to square
-    gameSquare.setAttribute('id', id)
+    // gameSquare.setAttribute('id', id)
 
+    let boxHelper = 'box' + index
     //adds square to array of square objects key=square# value= coordinates
-    idToCoords[id] = coordinateArrayOfSquares[index]
-
+    // squares.boxHelper['coordinates'] = coordinateArrayOfSquares[index]
     //add event listener to turn box green. can be modified to change to flipped
     gameSquare.addEventListener('click', () => {
       gameSquare.style.background = 'green'
-      alert(`My coordinates are ${coordinateArrayOfSquares[index]}`)
+      // alert(`My coordinates are ${coordinateArrayOfSquares[index]}`)
     })
   })
 }
